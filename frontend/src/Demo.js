f146f061e31c0aeae919f0b1d20dafe07ccb0aa4
Swag369 <86703988+Swag369@users.noreply.github.com>
2023-04-08 13:09:42 -0400

    Ishaan wrote mogodb auth backend
    Co-authored-by: Ishaan Ghosh <22ghoshi@users.noreply.github.com>

--- frontend/src/Demo.js
+++ frontend/src/Demo.js
@@ -1,14 +1,43 @@
-import React from 'react';
-import SideBar from './Components/SideBar.js';
-import './Demo.css';
+import React, { useState, useEffect } from "react";
+import { useLeaflet } from "react-leaflet";
 
 const Demo = () => {
+    const [map, setMap] = useState(null);
+    const [markers, setMarkers] = useState([]);
+
+    useEffect(() => {
+        // Load the California zipcode shapefile.
+        const shapefile = 'california_zipcodes.shp';
+
+        // Create a GeoJSON object for each zipcode.
+        const df = geopandas.read_file(shapefile);
+        const geojson = df.to_json();
+
+        // Add the GeoJSON objects to the map.
+        setMap(
+        leaflet.Map({
+            center: [37.7752, -122.4189],
+            zoom: 10,
+            layers: [
+            leaflet.providers.Stamen('watercolor'),
+            leaflet.GeoJSON(geojson),
+            ],
+        })
+        );
+
+        // Add a marker for each zipcode.
+        for (const zipcode of df['zipcode']) {
+        const marker = leaflet.marker([df['longitude'][zipcode], df['latitude'][zipcode]]);
+        marker.bindPopup(df['zipcode'][marker.layer_id]);
+        setMarkers([...markers, marker]);
+        }
+    }, []);
+
     return (
-        <div className='demo'>
-            <SideBar />
-            <h1>Demo</h1>
+        <div>
+        <leafletMap ref={setMap} markers={markers} />
         </div>
     );
-}
+};
 
 export default Demo;
