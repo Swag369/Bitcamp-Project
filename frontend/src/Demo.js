632fb800da87f75f453a855acf11b4029a6f5858
Swag369 <86703988+Swag369@users.noreply.github.com>
2023-04-08 16:20:01 -0400

    zip codes are now visible

--- frontend/src/Demo.js
+++ frontend/src/Demo.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useLeaflet, MapContainer, TileLayer, Marker, Popup, useMap, GeoJSON, LayerStyle, polygonData, onEachContry } from "react-leaflet";
 import 'leaflet/dist/leaflet.css'
+import zip_code_data from './California_ZipCodes/cali_zip_codes.json'
 
 import { Bounds } from "leaflet";
 
@@ -8,7 +9,7 @@ const Demo = () => {
 
     const position = [37.42, -122.16];
 
-    
+    const zip_code_style = {};
 
 
 
@@ -16,15 +17,20 @@ const Demo = () => {
 		<div style = {{height: "50vh"}}>
 
 
+            <div>Start</div>
 
-			<MapContainer center = {position} zoom={13} style = {{height: 600, width : "70vw"}} bounds = {[[42.036346, -124.924605], [32.189861, -112.755379]]}>
+
+			<MapContainer center = {position} zoom={5} style = {{height: 600, width : "70vw"}} bounds = {[[42.036346, -124.924605], [32.189861, -112.755379]]}>
 
 				<TileLayer
 					attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 					url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
 				/>
 
-
+                <GeoJSON
+                    data = {zip_code_data}
+                    style = {zip_code_style}
+                />
 
 			</MapContainer>
 
